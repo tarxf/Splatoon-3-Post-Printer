8c34fb22e1bc8756aedfd7bfca5b6b1165581dd7
Tar XF <tarxf@esquibits.com>
2022-09-11 19:21:18 -0700

    fixed a python break
    Newer python versions do not take well expanding division operations
    directly in an integer field, even if the result will be int.
    
    Signed-off-by: Tar XF <tarxf@esquibits.com>

--- bin2c.py
+++ bin2c.py
@@ -16,7 +16,7 @@ def main(argv):
   data = open(args[0], 'rb').read()
 
   str_out = "#include <stdint.h>\n#include <avr/pgmspace.h>\n\nconst uint8_t image_data[0x12c1] PROGMEM = {"
-  for i in range(0, (320*120) / 8):
+  for i in range(0, 4800):          # (320*120)/8
     val = 0;
 
     for j in range(0, 8):
